90b3fe746c4e3042fa0b2324931c9b0393903bb6
liuyang <2430237082@qq.com>
2020-10-30 13:44:38 +0800

    求n的k次方

---
+++ ckass92_1030b/ckass92_1030b/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<windows.h>
+int fun(int n, int k)     //求n的k次方
+{
+	int sum;
+	if (k == 0)
+	{
+		sum = 1;
+	}
+	else
+	{
+		sum = n*fun(n, k - 1);
+	}
+	return sum;
+}
+int main()
+{
+	printf("%d\n", fun(2, 3));
+	system("pause");
+	return 0;
+}
