60c9bea71b989aacbfaa7389d6e42842b12caa80
liuyang <2430237082@qq.com>
2020-11-10 22:18:43 +0800

    1

---
+++ class92_1110/class92_1110/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+void Reverse(char* str)
+{//×Ö·û´®ÄæÐò
+	char* left = str;
+	char* right = str + strlen(str) - 1;
+	while (left < right)
+	{
+		char temp = *left;
+		*left = *right;
+		*right = temp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	char str[] = "hello bit";
+	Reverse(str);
+	printf("%s\n",str);
+	system("pause");
+	return 0;
+}
