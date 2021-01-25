b7cdee2f247769c54636133a42241b60c6438976
liuyang <2430237082@qq.com>
2021-01-25 22:39:35 +0800

    1-100所有偶数和

---
+++ evennumbersum/EvenNumberSum.java
@@ -0,0 +1,15 @@
+package HomeWork;
+
+public class EvenNumberSum {
+        //        求出1-100之间的偶数和
+        public static void main(String[] args) {
+            int sum=0;
+            for (int i = 1; i <= 100; i++) {
+                if (i%2==0){
+                    sum+=i;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+
