229944363d91aaf61bceae133a41dddb86d63fa3
liuyang <2430237082@qq.com>
2021-03-26 19:09:49 +0800

    string类一些操作

---
+++ myarraylist/Split.java
@@ -0,0 +1,30 @@
+package Java_0326;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Split {
+    public static void main(String[] args) {
+        String a = "hello world java";
+       // String[] result = a.split(" ");
+       // System.out.println(Arrays.toString(result));
+        //String result1 = a.replaceAll("h","q");
+       // System.out.println(result1);
+       // String result2 = a.replace("e","f");
+       // System.out.println(result2);
+       // String result3 = a.replaceFirst("l","y");
+       // System.out.println(result3);
+        //System.out.println(a.indexOf("world"));
+       // System.out.println(a.indexOf("w"));
+        //System.out.println(a.indexOf("l"));
+        //System.out.println(a.indexOf("l",4));
+        //System.out.println(a.lastIndexOf("l"));
+        System.out.println("A".compareTo("a"));
+        System.out.println("a".compareTo("A"));
+        System.out.println("A".compareTo("A"));
+        System.out.println("AB".compareTo("AC"));
+        System.out.println("刘".compareTo("罗"));
+
+
+    }
+}
